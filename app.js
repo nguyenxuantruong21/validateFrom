c25a325ac1caeb990e36c444996a2b6cf20cdcd7
Nguyen Truong <xuantruong200101@gmai.com>
2023-03-08 16:24:32 +0700

    xuan truong

---
+++ app.js
@@ -0,0 +1,29 @@
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let flag = 1
+
+function validateFrom() {
+    if (username.value === '') {
+        document.getElementById('userError').innerHTML = "user name is empty"
+        flag = 0
+    } else if (username.value.length < 3) {
+        document.getElementById('userError').innerHTML = 'user name require min 3 char'
+        flag = 0
+    } else {
+        document.getElementById('userError').innerHTML = ''
+        flag = 1
+    }
+
+    if (password.value == '') {
+        document.getElementById('passError').innerHTML = 'password is empty'
+        flag = 0
+    } else {
+        document.getElementById('passError').innerHTML = ''
+    }
+
+    if (flag) {
+        return true
+    } else {
+        return false
+    }
+}
